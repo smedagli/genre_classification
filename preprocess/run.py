8a9a5c757bbdd54ec1d4311e5bf8da059cba2bc6
smedagli <s.medagli@gmail.com>
2022-07-19 13:45:24 +0200

    refactor run.py

--- preprocess/run.py
+++ preprocess/run.py
@@ -47,34 +47,19 @@ def go(args):
     os.remove(filename)
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Preprocess a dataset", fromfile_prefix_chars="@")
+
+    parser.add_argument("--input_artifact", type=str, required=True,
+                        help="Fully-qualified name for the input artifact")
+    parser.add_argument("--artifact_name", type=str, help="Name for the artifact", required=True)
+    parser.add_argument("--artifact_type", type=str, help="Type for the artifact", required=True)
+    parser.add_argument("--artifact_description", type=str, required=True,
+                        help="Description for the artifact")
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Preprocess a dataset",
-        fromfile_prefix_chars="@",
-    )
-
-    parser.add_argument(
-        "--input_artifact",
-        type=str,
-        help="Fully-qualified name for the input artifact",
-        required=True,
-    )
-
-    parser.add_argument(
-        "--artifact_name", type=str, help="Name for the artifact", required=True
-    )
-
-    parser.add_argument(
-        "--artifact_type", type=str, help="Type for the artifact", required=True
-    )
-
-    parser.add_argument(
-        "--artifact_description",
-        type=str,
-        help="Description for the artifact",
-        required=True,
-    )
-
-    args = parser.parse_args()
-
+    args = parse_args()
     go(args)
